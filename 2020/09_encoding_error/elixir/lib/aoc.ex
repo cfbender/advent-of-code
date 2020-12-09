c8f880cac8fc17292de064056f7e69b764eec2f5
Cody Bender <cody.bender@pdq.com>
2020-12-09 00:54:42 -0700

    style: format day 9 ex

--- 2020/09_encoding_error/elixir/lib/aoc.ex
+++ 2020/09_encoding_error/elixir/lib/aoc.ex
@@ -5,24 +5,28 @@ defmodule AoC do
   end
 
   def part1(nums) do
-    nums 
+    nums
     |> Enum.chunk_every(26, 1, :discard)
-    |> Enum.find(fn x -> 
-       combos = for  y <- x, z <- x, y != z, do: [y,z]
-      |> Enum.sum()
-      
+    |> Enum.find(fn x ->
+      combos =
+        for y <- x,
+            z <- x,
+            y != z,
+            do:
+              [y, z]
+              |> Enum.sum()
+
       !Enum.member?(combos, List.last(x))
     end)
     |> List.last()
   end
 
   def part2(nums) do
-
   end
 
   def main do
     input = get_input()
-    IO.puts "Part 1: #{part1(input)}"
+    IO.puts("Part 1: #{part1(input)}")
   end
 
   def run1 do
