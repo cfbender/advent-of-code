67279888491bbf1ec5bbc24ad96bd4ee3d1f1c0f
Cody Bender <cody.bender@pdq.com>
2020-12-08 21:52:32 -0700

    chore: change module name for standardize

--- 2020/01_report_repair/elixir/test/report_repair_test.exs
+++ 2020/01_report_repair/elixir/test/report_repair_test.exs
@@ -2,7 +2,7 @@ defmodule ReportRepairTest do
   use ExUnit.Case
 
   test "Parts" do
-    assert ReportRepair.run1() == 935_419
-    assert ReportRepair.run2() == 49_880_012
+    assert AoC.run1() == 935_419
+    assert AoC.run2() == 49_880_012
   end
 end
