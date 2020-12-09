2d56e0b2775b39333c82fb92194bc92927fb5316
Cody Bender <cody.bender@pdq.com>
2020-12-08 21:55:58 -0700

    fix: get yer typescript off my lawn

--- 2020/01_report_repair/elixir/lib/report_repair.ex
+++ 2020/01_report_repair/elixir/lib/report_repair.ex
@@ -1,25 +1,3 @@
-# const input = await Deno.readTextFile('../input.txt')
-# const lines = input.split(/\r?\n/).map((num) => parseInt(num))
-# const reducePairs = (limit: number) =>
-# lines.reduce(
-# (acc, curr) =>
-# curr < limit && lines.includes(limit - curr) ? acc * curr : acc,
-# 1
-# )
-
-# const tripletResult = lines.reduce((acc, curr) => {
-# if (curr < 2020) {
-# const pairQuotient = reducePairs(2020 - curr)
-# if (pairQuotient > 1) {
-# return curr * pairQuotient
-# }
-# }
-# return acc
-# })
-
-# console.log(`Pair result: ${reducePairs(2020)}`)
-# console.log(`Triplet result: ${tripletResult}`)
-
 defmodule AoC do
   def get_input do
     {:ok, input} = File.read("../input.txt")
