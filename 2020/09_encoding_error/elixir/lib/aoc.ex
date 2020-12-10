676b3342bbccf2f29e19922bd3693103800fdb31
Cody Bender <cody.bender@pdq.com>
2020-12-09 20:11:37 -0700

    chore: exliri day 9 part 2

--- 2020/09_encoding_error/elixir/lib/aoc.ex
+++ 2020/09_encoding_error/elixir/lib/aoc.ex
@@ -21,12 +21,39 @@ defmodule AoC do
     |> List.last()
   end
 
-  def part2(nums) do
+  def list_check(list, check, i, full_list) do
+    sum = Enum.sum(list)
+
+    if sum == check do
+      list
+      |> Enum.min_max()
+      |> Tuple.to_list()
+      |> Enum.sum()
+    else
+      cond do
+        sum < check ->
+          list ++ [Enum.at(full_list, i + 1)]
+          |> list_check(check, i + 1, full_list)
+        true ->
+          List.delete_at(list, 0)
+          |> list_check(check, i, full_list)
+      end
+    end
+  end
+
+  def part2(nums, check) do
+    nums
+    |> List.first()
+    |> List.wrap()
+    |> list_check(check, 0, nums)
   end
 
   def main do
     input = get_input()
-    IO.puts("Part 1: #{part1(input)}")
+    result1 = part1(input)
+    IO.puts("Part 1: #{result1}")
+    result2 = part2(input, result1)
+    IO.puts("Part 1: #{result2}")
   end
 
   def run1 do
@@ -36,6 +63,7 @@ defmodule AoC do
 
   def run2 do
     input = get_input()
-    part2(input)
+    result = part1(input)
+    part2(input, result)
   end
 end
