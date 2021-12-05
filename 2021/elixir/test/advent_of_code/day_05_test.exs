b4c11bc5d68720d7f9a93114db018ebd5fc0be4d
Cody Bender <cody.bender@pdq.com>
2021-12-04 23:38:54 -0700

    chore: aoc 2021 day 5

--- 2021/elixir/test/advent_of_code/day_05_test.exs
+++ 2021/elixir/test/advent_of_code/day_05_test.exs
@@ -3,19 +3,41 @@ defmodule AdventOfCode.Day05Test do
 
   import AdventOfCode.Day05
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = [
+      "0,9 -> 5,9",
+      "8,0 -> 0,8",
+      "9,4 -> 3,4",
+      "2,2 -> 2,1",
+      "7,0 -> 7,4",
+      "6,4 -> 2,0",
+      "0,9 -> 2,9",
+      "3,4 -> 1,4",
+      "0,0 -> 8,8",
+      "5,5 -> 8,2"
+    ]
+
     result = part1(input)
 
-    assert result
+    assert result == 5
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = [
+      "0,9 -> 5,9",
+      "8,0 -> 0,8",
+      "9,4 -> 3,4",
+      "2,2 -> 2,1",
+      "7,0 -> 7,4",
+      "6,4 -> 2,0",
+      "0,9 -> 2,9",
+      "3,4 -> 1,4",
+      "0,0 -> 8,8",
+      "5,5 -> 8,2"
+    ]
+
     result = part2(input)
 
-    assert result
+    assert result == 12
   end
 end
