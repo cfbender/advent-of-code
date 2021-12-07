ad3398beda01dc286adec487b6ef13503515377a
Cody Bender <cody.bender@pdq.com>
2021-12-06 22:38:48 -0700

    chore: aoc 2021 day 7

--- 2021/elixir/lib/advent_of_code/day_07.ex
+++ 2021/elixir/lib/advent_of_code/day_07.ex
@@ -1,7 +1,30 @@
 defmodule AdventOfCode.Day07 do
-  def part1(_args) do
+  def part1(input) do
+    sorted = Enum.sort(input)
+    median = Enum.at(sorted, Integer.floor_div(length(input), 2))
+
+    Enum.reduce(sorted, 0, &(abs(&1 - median) + &2))
   end
 
-  def part2(_args) do
+  def move_crabs(crabs, destination, previous \\ Infinity) do
+    cost =
+      Enum.reduce(crabs, 0, fn curr, acc ->
+        distance = abs(curr - destination)
+        # (n^2 + n) / 2 (nth triangle number)
+        acc + Integer.floor_div(Integer.pow(distance, 2) + distance, 2)
+      end)
+
+    if(cost > previous) do
+      previous
+    else
+      move_crabs(crabs, destination + 1, cost)
+    end
+  end
+
+  def part2(input) do
+    sorted = Enum.sort(input)
+    median = Enum.at(sorted, Integer.floor_div(length(input), 2))
+
+    move_crabs(sorted, median)
   end
 end
