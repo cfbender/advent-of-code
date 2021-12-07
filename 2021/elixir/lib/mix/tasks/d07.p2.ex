d17cac1035542945b45be2ce1f63cb92495b42dd
Cody Bender <cody.bender@pdq.com>
2021-12-06 22:38:48 -0700

    chore: aoc 2021 day 7

--- 2021/elixir/lib/mix/tasks/d07.p2.ex
+++ 2021/elixir/lib/mix/tasks/d07.p2.ex
@@ -5,7 +5,11 @@ defmodule Mix.Tasks.D07.P2 do
 
   @shortdoc "Day 07 Part 2"
   def run(args) do
-    input = nil
+    input =
+      AdventOfCode.Input.get!(7, 2021)
+      |> String.trim()
+      |> String.split(",")
+      |> Enum.map(&String.to_integer/1)
 
     if Enum.member?(args, "-b"),
       do: Benchee.run(%{part_2: fn -> input |> part2() end}),
--- 2021/elixir/test/advent_of_code/day_07_test.exs
+++ 2021/elixir/test/advent_of_code/day_07_test.exs
@@ -3,19 +3,17 @@ defmodule AdventOfCode.Day07Test do
 
   import AdventOfCode.Day07
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
     result = part1(input)
 
-    assert result
+    assert result == 37
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
     result = part2(input)
 
-    assert result
+    assert result == 168
   end
 end
