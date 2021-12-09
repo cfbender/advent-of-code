d83a37443b208deff56fc40cecacb7672bddbd66
Cody Bender <cody.bender@pdq.com>
2021-12-09 00:14:35 -0700

    chore: aoc 2021 day 9

--- 2021/elixir/test/advent_of_code/day_09_test.exs
+++ 2021/elixir/test/advent_of_code/day_09_test.exs
@@ -3,19 +3,31 @@ defmodule AdventOfCode.Day09Test do
 
   import AdventOfCode.Day09
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = [
+      "2199943210",
+      "3987894921",
+      "9856789892",
+      "8767896789",
+      "9899965678"
+    ]
+
     result = part1(input)
 
-    assert result
+    assert result == 15
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = [
+      "2199943210",
+      "3987894921",
+      "9856789892",
+      "8767896789",
+      "9899965678"
+    ]
+
     result = part2(input)
 
-    assert result
+    assert result == 1134
   end
 end
