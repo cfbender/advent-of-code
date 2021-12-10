df8178fa591bd6fcc8d38a4e577640eb68aa67df
Cody Bender <cody.bender@pdq.com>
2021-12-09 22:38:58 -0700

    chore: aoc 2021 day 10

--- 2021/elixir/lib/advent_of_code/day_10.ex
+++ 2021/elixir/lib/advent_of_code/day_10.ex
@@ -1,7 +1,78 @@
 defmodule AdventOfCode.Day10 do
-  def part1(_args) do
+  @openers ["(", "[", "{", "<"]
+  @closers [
+    ")",
+    "]",
+    "}",
+    ">"
+  ]
+
+  @pairs for x <- 0..3, into: %{}, do: {Enum.at(@openers, x), Enum.at(@closers, x)}
+
+  @corrupt_points %{
+    ")" => 3,
+    "]" => 57,
+    "}" => 1197,
+    ">" => 25137
+  }
+
+  @complete_points %{
+    ")" => 1,
+    "]" => 2,
+    "}" => 3,
+    ">" => 4
+  }
+
+  def parse_line(line) do
+    String.codepoints(line)
+    |> Enum.reduce_while([], fn curr, stack ->
+      if Enum.member?(@closers, curr) do
+        if Map.get(@pairs, List.first(stack)) == curr do
+          # closed successfully
+          [_prev | rest] = stack
+          {:cont, rest}
+        else
+          {:halt, curr}
+        end
+      else
+        {:cont, [curr | stack]}
+      end
+    end)
   end
 
-  def part2(_args) do
+  def part1(input) do
+    input
+    |> Enum.reduce(0, fn line, acc ->
+      result = parse_line(line)
+
+      if is_list(result) do
+        acc
+      else
+        acc + Map.get(@corrupt_points, result)
+      end
+    end)
+  end
+
+  def part2(input) do
+    scores =
+      input
+      |> Enum.reduce([], fn line, acc ->
+        result = parse_line(line)
+
+        if is_list(result) do
+          score =
+            Enum.map(result, &Map.get(@pairs, &1))
+            |> Enum.reduce(0, fn curr, total ->
+              total * 5 + Map.get(@complete_points, curr)
+            end)
+
+          [score | acc]
+        else
+          acc
+        end
+      end)
+      |> Enum.sort()
+
+    Enum.at(scores, floor(length(scores) / 2))
   end
 end
