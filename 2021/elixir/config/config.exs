d16813a690c34b4d5b9b9c16526c1c455ebc98b2
Cody Bender <cody.bender@pdq.com>
2021-12-09 22:42:38 -0700

    chore: don't use deprecated Mix.Config

--- 2021/elixir/config/config.exs
+++ 2021/elixir/config/config.exs
@@ -1,12 +1,14 @@
-use Mix.Config
+import Config
 
 config :advent_of_code, AdventOfCode.Input, allow_network?: true
 
 # If you don't like environment variables, put your cookie in
 # a `config/secrets.exs` file like this:
 #
+# import Config
+#
 # config :advent_of_code, AdventOfCode.Input,
-#   advent_of_code_session_cookie: "session=..."
+#  session_cookie: "..."
 
 try do
   import_config "secrets.exs"
