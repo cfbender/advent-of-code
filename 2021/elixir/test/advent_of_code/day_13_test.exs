27f2013be6046e5de29dc97b8178636943fb1cc6
cfbender <codyfbender@gmail.com>
2021-12-12 23:19:54 -0700

    chore: aoc 2021 day 13

--- 2021/elixir/test/advent_of_code/day_13_test.exs
+++ 2021/elixir/test/advent_of_code/day_13_test.exs
@@ -3,17 +3,67 @@ defmodule AdventOfCode.Day13Test do
 
   import AdventOfCode.Day13
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = [
+      [
+        "6,10",
+        "0,14",
+        "9,10",
+        "0,3",
+        "10,4",
+        "4,11",
+        "6,0",
+        "6,12",
+        "4,1",
+        "0,13",
+        "10,12",
+        "3,4",
+        "3,0",
+        "8,4",
+        "1,10",
+        "2,14",
+        "8,10",
+        "9,0"
+      ],
+      [
+        "fold along y=7",
+        "fold along x=5"
+      ]
+    ]
+
     result = part1(input)
 
-    assert result
+    assert result == 17
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = [
+      [
+        "6,10",
+        "0,14",
+        "9,10",
+        "0,3",
+        "10,4",
+        "4,11",
+        "6,0",
+        "6,12",
+        "4,1",
+        "0,13",
+        "10,12",
+        "3,4",
+        "3,0",
+        "8,4",
+        "1,10",
+        "2,14",
+        "8,10",
+        "9,0"
+      ],
+      [
+        "fold along y=7",
+        "fold along x=5"
+      ]
+    ]
+
     result = part2(input)
 
     assert result
