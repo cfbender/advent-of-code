f4241dbea16c9dde33e2a6dabb02502ade554b14
Cody Bender <cody.bender@pdq.com>
2021-12-15 09:07:13 -0700

    chore: aoc 2021 day 15

--- 2021/elixir/lib/mix/tasks/d15.p1.ex
+++ 2021/elixir/lib/mix/tasks/d15.p1.ex
@@ -5,7 +5,10 @@ defmodule Mix.Tasks.D15.P1 do
 
   @shortdoc "Day 15 Part 1"
   def run(args) do
-    input = nil
+    input =
+      AdventOfCode.Input.get!(15, 2021)
+      |> String.split("\n", trim: true)
+      |> process_input()
 
     if Enum.member?(args, "-b"),
       do: Benchee.run(%{part_1: fn -> input |> part1() end}),
