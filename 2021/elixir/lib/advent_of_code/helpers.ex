16b5b3d003e2e8403db42127d7cd33eff8f4e4e3
Cody Bender <cody.bender@pdq.com>
2021-12-19 23:48:06 -0700

    chore: aoc 2021 day 20

--- 2021/elixir/lib/advent_of_code/helpers.ex
+++ 2021/elixir/lib/advent_of_code/helpers.ex
@@ -26,7 +26,10 @@ defmodule AdventOfCode.Helpers do
       iex> Helpers.get_adj(%{{0, 0} => 1, {0, 1} => 4, {1, 0} => 2, {1, 1} => 5, {2, 0} => 3, {2, 1} => 6}, {1,1})
       %{{0, 0} => 1, {0, 1} => 4, {1, 0} => 2, {1, 1} => 5, {2, 0} => 3, {2, 1} => 6}
   """
-  def get_adj(map, {x, y}, [all: all] \\ [all: true]) do
+  def get_adj(map, {x, y}, opts \\ []) do
+    default = Keyword.get(opts, :default, nil)
+    all = Keyword.get(opts, :all, true)
+
     deltas =
       if all,
         do: for(x <- [-1, 0, 1], y <- [-1, 0, 1], do: {x, y}),
@@ -36,7 +39,7 @@ defmodule AdventOfCode.Helpers do
     |> Enum.reduce(Map.new(), fn {dx, dy}, acc ->
       new_point = {x + dx, y + dy}
 
-      new_point_val = Map.get(map, new_point)
+      new_point_val = Map.get(map, new_point, default)
 
       if new_point_val do
         Map.put(acc, new_point, new_point_val)
