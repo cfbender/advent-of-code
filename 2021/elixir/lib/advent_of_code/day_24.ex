b528554898629c4f4072ef042a8e5a984c579228
Cody Bender <cody.bender@pdq.com>
2021-12-24 13:19:56 -0700

    chore: aoc 2021 day 24

--- 2021/elixir/lib/advent_of_code/day_24.ex
+++ 2021/elixir/lib/advent_of_code/day_24.ex
@@ -1,7 +1,58 @@
 defmodule AdventOfCode.Day24 do
-  def part1(_args) do
+  def parse_function_parameters(program) do
+    # get each section of program
+    Enum.chunk_every(program, 18)
+    |> Enum.map(fn section ->
+      # get 3 unique values as parameters
+      for(
+        x <- [4, 5, 15],
+        do: Enum.at(section, x) |> String.split(" ") |> List.last() |> String.to_integer()
+      )
+      |> List.to_tuple()
+    end)
   end
 
-  def part2(_args) do
+  # https://www.reddit.com/r/adventofcode/comments/rnejv5/2021_day_24_solutions/hpsv5hl/?context=3
+  def monad({a, b, _c}, z, w) when rem(z, 26) + b == w, do: Integer.floor_div(z, a)
+  def monad({a, _b, c}, z, w), do: Integer.floor_div(z, a) * 26 + w + c
+
+  def find_highest(params_list, zs \\ %{0 => {0, 0}})
+  def find_highest([], zs), do: zs
+
+  def find_highest([params = {a, _, _} | rest_params], zs) do
+    new_zs =
+      Enum.reduce(zs, Map.new(), fn {z, {min, max}}, z_acc ->
+        Enum.reduce(1..9, z_acc, fn digit, new_z_acc ->
+          # map all possible next digits to new z value
+          new_z = monad(params, z, digit)
+
+          if a == 1 or (a == 26 and new_z < z) do
+            {new_min, new_max} = {min * 10 + digit, max * 10 + digit}
+
+            Map.update(new_z_acc, new_z, {new_min, new_max}, fn {curr_min, curr_max} ->
+              {min(new_min, curr_min), max(new_max, curr_max)}
+            end)
+          else
+            # skip function call that won't help
+            new_z_acc
+          end
+        end)
+      end)
+
+    find_highest(rest_params, new_zs)
+  end
+
+  def part1(input) do
+    parse_function_parameters(input)
+    |> find_highest()
+    |> Map.get(0)
+    |> elem(1)
+  end
+
+  def part2(input) do
+    parse_function_parameters(input)
+    |> find_highest()
+    |> Map.get(0)
+    |> elem(0)
   end
 end
