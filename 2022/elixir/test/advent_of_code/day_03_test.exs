f734685c6e01104546952beaf0bf9343bea9835f
Cody Bender <cody.bender@pdq.com>
2022-12-03 00:44:56 -0500

    chore: day 3 2022 ex

--- 2022/elixir/test/advent_of_code/day_03_test.exs
+++ 2022/elixir/test/advent_of_code/day_03_test.exs
@@ -6,22 +6,34 @@ defmodule AdventOfCode.Day03Test do
   test "part1" do
     input =
       """
+      vJrwpWtwJgWrhcsFMMfFFhFp
+      jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+      PmmdzqPrVvPwwTWBwg
+      wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+      ttgJtRGJQctTZtZT
+      CrZsJsPPZsGzwwsLwLmpwMDw
       """
       |> parse_input()
 
     result = part1(input)
 
-    assert result
+    assert result == 157
   end
 
   test "part2" do
     input =
       """
+      vJrwpWtwJgWrhcsFMMfFFhFp
+      jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+      PmmdzqPrVvPwwTWBwg
+      wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+      ttgJtRGJQctTZtZT
+      CrZsJsPPZsGzwwsLwLmpwMDw
       """
       |> parse_input()
 
     result = part2(input)
 
-    assert result
+    assert result == 70
   end
 end
