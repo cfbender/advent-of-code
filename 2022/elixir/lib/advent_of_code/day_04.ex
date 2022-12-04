cde54ba39b45115dcff8f5cfe669ba190c64c37f
Cody Bender <cody.bender@pdq.com>
2022-12-04 00:15:15 -0500

    chore: 2022 day 4 ex

--- 2022/elixir/lib/advent_of_code/day_04.ex
+++ 2022/elixir/lib/advent_of_code/day_04.ex
@@ -1,7 +1,35 @@
 defmodule AdventOfCode.Day04 do
-  def part1(_args) do
+  @moduledoc """
+  """
+  def parse_input(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      String.split(line, ",")
+      |> Enum.map(fn assign ->
+        [start, ending] =
+          String.split(assign, "-")
+          |> Enum.map(&String.to_integer/1)
+
+        start..ending |> MapSet.new()
+      end)
+    end)
   end
 
-  def part2(_args) do
+  def part1(input) do
+    input
+    |> Stream.filter(fn [set1, set2] ->
+      MapSet.subset?(set1, set2) || MapSet.subset?(set2, set1)
+    end)
+    |> Enum.count()
+  end
+
+  def part2(input) do
+    input
+    |> Stream.filter(fn [set1, set2] ->
+      MapSet.intersection(set1, set2)
+      |> MapSet.size() > 0
+    end)
+    |> Enum.count()
   end
 end
--- 2022/elixir/test/advent_of_code/day_04_test.exs
+++ 2022/elixir/test/advent_of_code/day_04_test.exs
@@ -3,19 +3,37 @@ defmodule AdventOfCode.Day04Test do
 
   import AdventOfCode.Day04
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input =
+      """
+      2-4,6-8
+      2-3,4-5
+      5-7,7-9
+      2-8,3-7
+      6-6,4-6
+      2-6,4-8
+      """
+      |> parse_input()
+
     result = part1(input)
 
-    assert result
+    assert result == 2
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input =
+      """
+      2-4,6-8
+      2-3,4-5
+      5-7,7-9
+      2-8,3-7
+      6-6,4-6
+      2-6,4-8
+      """
+      |> parse_input()
+
     result = part2(input)
 
-    assert result
+    assert result == 4
   end
 end
