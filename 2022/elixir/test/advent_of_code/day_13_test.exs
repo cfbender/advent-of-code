5492c3f28ec1db29838ac9857720c3c12db0e446
Cody Bender <cody.bender@pdq.com>
2022-12-13 11:04:27 -0500

    chore: finish 2022 day 13 ex

--- 2022/elixir/test/advent_of_code/day_13_test.exs
+++ 2022/elixir/test/advent_of_code/day_13_test.exs
@@ -37,15 +37,37 @@ defmodule AdventOfCode.Day13Test do
     assert result == 13
   end
 
-  @tag :skip
   test "part2" do
     input =
       """
+      [1,1,3,1,1]
+      [1,1,5,1,1]
+
+      [[1],[2,3,4]]
+      [[1],4]
+
+      [9]
+      [[8,7,6]]
+
+      [[4,4],4,4]
+      [[4,4],4,4,4]
+
+      [7,7,7,7]
+      [7,7,7]
+
+      []
+      [3]
+
+      [[[]]]
+      [[]]
+
+      [1,[2,[3,[4,[5,6,7]]]],8,9]
+      [1,[2,[3,[4,[5,6,0]]]],8,9]
       """
       |> parse_input()
 
     result = part2(input)
 
-    assert result
+    assert result == 140
   end
 end
