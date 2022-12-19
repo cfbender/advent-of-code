bcf0dbbdf9c1bd20eea0ba98fe46632677c345ab
Cody Bender <cody.bender@pdq.com>
2022-12-19 18:03:08 -0500

    wip: 2022 day 19 ex

--- 2022/elixir/test/advent_of_code/day_19_test.exs
+++ 2022/elixir/test/advent_of_code/day_19_test.exs
@@ -3,16 +3,18 @@ defmodule AdventOfCode.Day19Test do
 
   import AdventOfCode.Day19
 
-  @tag :skip
+  @tag timeout: :infinity
   test "part1" do
     input =
       """
+      Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
+      Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.
       """
       |> parse_input()
 
     result = part1(input)
 
-    assert result
+    assert result == 33
   end
 
   @tag :skip
