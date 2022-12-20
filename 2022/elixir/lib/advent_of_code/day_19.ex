0b48b041e184070ad7152c49029cd79f154464c6
Cody Bender <cody.bender@pdq.com>
2022-12-20 01:55:23 -0500

    --wip-- [skip ci]

--- 2022/elixir/lib/advent_of_code/day_19.ex
+++ 2022/elixir/lib/advent_of_code/day_19.ex
@@ -53,20 +53,24 @@ defmodule AdventOfCode.Day19 do
   def can_build?(
         %Blueprint{geode: {ore_cost, obisidian_cost}},
         %{ore: ore, obsidian: obsidian},
-        :geode
+        :geode_robot
       ),
       do: ore >= ore_cost and obsidian >= obisidian_cost
 
-  def can_build?(%Blueprint{obsidian: {ore_cost, clay_cost}}, %{ore: ore, clay: clay}, :obsidian),
-    do: ore >= ore_cost and clay >= clay_cost
+  def can_build?(
+        %Blueprint{obsidian: {ore_cost, clay_cost}},
+        %{ore: ore, clay: clay},
+        :obsidian_robot
+      ),
+      do: ore >= ore_cost and clay >= clay_cost
 
-  def can_build?(%Blueprint{clay: ore_cost}, %{ore: ore}, :clay),
+  def can_build?(%Blueprint{clay: ore_cost}, %{ore: ore}, :clay_robot),
     do: ore >= ore_cost
 
-  def can_build?(%Blueprint{ore: ore_cost}, %{ore: ore}, :ore),
+  def can_build?(%Blueprint{ore: ore_cost}, %{ore: ore}, :ore_robot),
     do: ore >= ore_cost
 
-  def build(%Blueprint{geode: {g_ore, g_obs}}, :geode) do
+  def build(%Blueprint{geode: {g_ore, g_obs}}, :geode_robot) do
     fn state ->
       %{
         state
@@ -77,7 +81,7 @@ defmodule AdventOfCode.Day19 do
     end
   end
 
-  def build(%Blueprint{obsidian: {obs_ore, obs_clay}}, :obsidian) do
+  def build(%Blueprint{obsidian: {obs_ore, obs_clay}}, :obsidian_robot) do
     fn state ->
       %{
         state
@@ -88,26 +92,39 @@ defmodule AdventOfCode.Day19 do
     end
   end
 
-  def build(blueprint, :clay) do
+  def build(blueprint, :clay_robot) do
     fn state -> %{state | ore: state.ore - blueprint.clay, clay_robot: state.clay_robot + 1} end
   end
 
-  def build(blueprint, :ore) do
+  def build(blueprint, :ore_robot) do
     fn state -> %{state | ore: state.ore - blueprint.ore, ore_robot: state.ore_robot + 1} end
   end
 
   def try_builds(
         state,
-        blueprint
+        blueprint,
+        limits
       ) do
     # start with list that skips the build
-    Enum.reduce([:geode, :obsidian, :clay, :ore], [fn state -> state end], fn type, builds ->
-      if can_build?(blueprint, state, type) do
-        [build(blueprint, type) | builds]
-      else
-        builds
-      end
-    end)
+    start = [fn state -> state end]
+
+    cond do
+      can_build?(blueprint, state, :geode_robot) ->
+        [build(blueprint, :geode_robot)]
+
+      can_build?(blueprint, state, :obsidian_robot) and
+          state.obsidian_robot < limits.obsidian_robot ->
+        [build(blueprint, :obsidian_robot) | start]
+
+      true ->
+        Enum.reduce([:clay_robot, :ore_robot], start, fn type, builds ->
+          if can_build?(blueprint, state, type) and state[type] < limits[type] do
+            [build(blueprint, type) | builds]
+          else
+            builds
+          end
+        end)
+    end
   end
 
   def update_state(
@@ -133,18 +150,26 @@ defmodule AdventOfCode.Day19 do
     }
   end
 
-  def find_best(blueprint, queue, best \\ 0) do
+  def find_best(blueprint, queue, time_limit, limits, best \\ 0, best_for_time \\ %{}) do
     case Prioqueue.extract_min(queue) do
       {:error, :empty} ->
         best
 
       {:ok, {%{time: time} = curr_state, rest_queue}} ->
         new_best = max(curr_state.geode, best)
-        # should_drop = time > 20 and curr_state.geode == 0
+        curr_best_for_time = Map.get(best_for_time, time, 0)
 
-        if time < 24 do
+        new_best_for_time =
+          Map.put(best_for_time, time, max(curr_best_for_time, curr_state.geode))
+
+        # Drop a path if it's less than 50% the current best for that time
+        g_heuristic = 2 * curr_state.geode
+        # Only do this for the last 5 minutes
+        t_heuristic = time_limit - 5
+
+        if time < time_limit and (time < t_heuristic or g_heuristic >= curr_best_for_time) do
           # try builds with the starting state inventory of mats
-          builds = try_builds(curr_state, blueprint)
+          builds = try_builds(curr_state, blueprint, limits)
 
           # update the starting state for current robots and increment time
           new_state = update_state(curr_state)
@@ -155,26 +180,57 @@ defmodule AdventOfCode.Day19 do
               Prioqueue.insert(q, new_state |> build.())
             end)
 
-          find_best(blueprint, new_queue, new_best)
+          find_best(blueprint, new_queue, time_limit, limits, new_best, new_best_for_time)
         else
-          find_best(blueprint, rest_queue, new_best)
+          find_best(blueprint, rest_queue, time_limit, limits, new_best, new_best_for_time)
         end
     end
   end
 
+  @doc """
+  Limit the robots to the consumption cap of other robots
+  """
+  def robot_limits(%Blueprint{
+        ore: ore,
+        clay: c_ore,
+        obsidian: {o_ore, o_clay},
+        geode: {g_ore, g_obs}
+      }) do
+    %{
+      ore_robot: Enum.sum([ore, c_ore, o_ore, g_ore]),
+      clay_robot: o_clay,
+      obsidian_robot: g_obs
+    }
+  end
+
   def part1(input) do
-    # 1197 too low
     queue = Prioqueue.new([initial_state()], cmp_fun: &compare_states/2)
 
     Enum.map(input, fn %{number: n} = bp ->
+      limits = robot_limits(bp)
+
       Task.async(fn ->
-        dbg(n * find_best(bp, queue))
+        n * find_best(bp, queue, 24, limits)
       end)
     end)
     |> Task.await_many(:infinity)
     |> Enum.sum()
   end
 
-  def part2(_args) do
+  def part2(input) do
+    # 11760 too low
+    queue = Prioqueue.new([initial_state()], cmp_fun: &compare_states/2)
+
+    Enum.take(input, 3)
+    |> Enum.map(fn bp ->
+      limits = robot_limits(bp)
+
+      Task.async(fn ->
+        find_best(bp, queue, 32, limits)
+      end)
+    end)
+    |> Task.await_many(:infinity)
+    |> IO.inspect(charlists: :as_lists)
+    |> Enum.product()
   end
 end
--- 2022/elixir/lib/advent_of_code/day_20.ex
+++ 2022/elixir/lib/advent_of_code/day_20.ex
@@ -1,11 +1,69 @@
 defmodule AdventOfCode.Day20 do
   @moduledoc """
   """
+  import AdventOfCode.Helpers
+
   def parse_input(input) do
     input
+    |> lines()
+    |> Enum.map(&String.to_integer/1)
   end
 
-  def part1(_args) do
+  def move(list, length, num, idx) do
+    move = if num > length, do: num - length, else: num
+    new_idx = rem(idx + move, length - 1)
+
+    cond do
+      # subtract an extra one because lists aren't zero indexed from the end
+      new_idx < 0 -> new_idx - 1
+      new_idx > 0 -> new_idx
+      new_idx == 0 -> nil
+    end
+    |> case do
+      nil ->
+        List.delete_at(list, idx)
+        |> List.insert_at(length, num)
+
+      wrapped_idx ->
+        List.delete_at(list, idx)
+        |> List.insert_at(wrapped_idx, num)
+    end
+  end
+
+  def find_indexes(list, num) do
+    Enum.with_index(list)
+    |> Enum.group_by(fn {x, _i} -> x end, fn {_x, i} -> i end)
+    |> Map.get(num)
+
+    # |> tap(fn _ -> dbg(length(list)) end)
+  end
+
+  def mix(list, length) do
+    Enum.reduce(list, list, fn num, acc ->
+      dbg(num)
+      dbg(Enum.find_index(acc, &(&1 == num)))
+      dbg(Enum.find_index(acc, &(&1 == 4363)))
+      indexes = find_indexes(acc, num)
+
+      Enum.reduce(indexes, acc, fn i, i_acc ->
+        move(i_acc, length, num, i)
+      end)
+    end)
+  end
+
+  def part1(input) do
+    # 20016 too high
+    length = length(input) |> dbg()
+
+    dbg(Enum.find_index(input, &(&1 == 4363)))
+    result = mix(input, length)
+    dbg(length(result))
+    zero = Enum.find_index(result, &(&1 == 0))
+
+    Enum.map([1000, 2000, 3000], fn x ->
+      Enum.at(result, rem(zero + x, length))
+    end)
+    |> Enum.sum()
   end
 
   def part2(_args) do
