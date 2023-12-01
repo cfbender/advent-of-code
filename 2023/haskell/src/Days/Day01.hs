1d58b6a5a16823fcaf5a751ea2b0cac72349ce32
Cody Bender <cody.bender@pdq.com>
2023-12-01 02:32:34 -0500

    chore(day 1): day 1 hs part 1

--- 2023/haskell/src/Days/Day01.hs
+++ 2023/haskell/src/Days/Day01.hs
@@ -1,6 +1,8 @@
 module Days.Day01 (runDay) where
 
 {- ORMOLU_DISABLE -}
+import Data.Text (replace, unpack, pack,  Text)
+import Data.Char
 import Data.List
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
@@ -12,7 +14,7 @@ import qualified Data.Vector as Vec
 import qualified Util.Util as U
 
 import qualified Program.RunDay as R (runDay, Day)
-import Data.Attoparsec.Text
+import Data.Attoparsec.Text 
 import Data.Void
 {- ORMOLU_ENABLE -}
 
@@ -21,19 +23,38 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = many1 (choice [letter, digit]) `sepBy` endOfLine
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [String]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
+ends list = [head list, last list]
+
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA = sum . map ((read :: String -> Int) . ends . filter isDigit)
 
 ------------ PART B ------------
+convert :: String -> String
+convert input = foldl (\acc (s, r) -> unpack . replace s r . pack $ acc) input digits
+  where
+    digits :: [(Text, Text)]
+    digits =
+      [ ("one", "1"),
+        ("two", "2"),
+        ("three", "3"),
+        ("four", "4"),
+        ("five", "5"),
+        ("six", "6"),
+        ("seven", "7"),
+        ("eight", "8"),
+        ("nine", "9"),
+        ("zero", "0")
+      ]
+
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = sum . map ((read :: String -> Int) . ends . filter isDigit . convert)
