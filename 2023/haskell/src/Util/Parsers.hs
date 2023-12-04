40e75f356818419d3b6edf617b42fb623b4a0aae
Cody Bender <cody.bender@pdq.com>
2023-12-04 00:59:03 -0500

    chore(2023): day 4 hs

--- 2023/haskell/src/Util/Parsers.hs
+++ 2023/haskell/src/Util/Parsers.hs
@@ -2,7 +2,7 @@ module Util.Parsers where
 
 import Data.Attoparsec.Text
 import Data.Map (Map)
-import qualified Data.Map as Map
+import Data.Map qualified as Map
 
 {-
 This module contains a list of parsers and combinators which are likely to be useful for Advent of Code problems.
