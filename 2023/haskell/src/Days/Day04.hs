c10a2188de7bf9e8e349ede85caab070c39fa951
Cody Bender <cody.bender@pdq.com>
2023-12-04 00:59:03 -0500

    chore(2023): day 4 hs

--- 2023/haskell/src/Days/Day04.hs
+++ 2023/haskell/src/Days/Day04.hs
@@ -1,19 +1,20 @@
 module Days.Day04 (runDay) where
 
+-- I liked this day! Though I feel like the explanations could've been a bit clearer.
+-- Maybe it's just hard to walk that line, because if he had said it was
+-- 1 * 2 * 2 * 2 for 4 winning numbers then it's too obvious it's 2^3? idk
+--
+-- Also got a little tripped up on the parsing (wasn't flexible enough with number of spaces)
+-- and the second part, but this was a fun one! Honestly wish every day this month
+-- was like the last two - but I know the storm is coming.
+
 {- ORMOLU_DISABLE -}
 import Data.List
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
 
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
-import Data.Void
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -21,19 +22,46 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = card `sepBy` endOfLine
+  where
+    numbers = decimal `sepBy` many1 space
+    card = do
+      string "Card"
+      many1 space
+      id <- decimal
+      string ":"
+      many1 space
+      winning <- numbers
+      space
+      string "|"
+      many1 space
+      myNumbers <- numbers
+      return (id, winning, myNumbers)
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [(Int, [Int], [Int])]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA =
+  sum
+    . map ((2 ^) . subtract 1 . length)
+    . filter (not . null)
+    . map (\(_, win, own) -> own `intersect` win)
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+addWinning :: Map Int Int -> Int -> [Int]
+addWinning m id = newCards ++ concatMap (addWinning m) newCards
+  where
+    -- get the count of matching numbers from the passed card
+    count = Map.findWithDefault 0 id m
+    newCards = [(id + 1) .. (id + count)]
+partB i = length cards + length (concatMap (addWinning cardMap . fst) cards)
+  where
+    cardMap = Map.fromList cards
+    cards = map (\(id, win, own) -> (id, length $ own `intersect` win)) i
