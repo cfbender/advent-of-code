a3f030fe5c10f1ac1daef1900c38a3417c0797bd
Cody Bender <cody.bender@pdq.com>
2023-12-09 01:08:10 -0500

    chore(2023: day 9 hs

--- 2023/haskell/src/Days/Day09.hs
+++ 2023/haskell/src/Days/Day09.hs
@@ -1,19 +1,18 @@
 module Days.Day09 (runDay) where
 
+-- a fun one! spent a lot of time fighting the compiler and types as usual
+-- and tried to be cheeky with only taking the last two before I realized
+-- that that only works up to the depth the pattern takes to resolve.
+-- so I just did it the easy way and then thought to myself why didn't I do that
+-- from the start!
+--
+-- really happy with how clean this solution came out though
+
 {- ORMOLU_DISABLE -}
-import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
+import Util.Util
 
 import qualified Program.RunDay as R (runDay, Day)
-import Data.Attoparsec.Text
-import Data.Void
+import Data.Attoparsec.Text 
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -21,19 +20,31 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = filter (not . null) . map reverse <$> ((signed decimal `sepBy` char ' ') `sepBy` endOfLine)
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [[Int]]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+
+difference (x : [y]) = x - y
+
+reduceLevel :: [Int] -> [Int]
+reduceLevel = map difference . windows 2
+
+findNext :: Int -> [Int] -> Int
+findNext n l
+  | all (== 0) l = n
+  | otherwise = findNext (n + head l) (reduceLevel l)
+
+partA = sum . map (findNext 0)
 
 ------------ PART B ------------
+
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = sum . map (findNext 0 . reverse)
