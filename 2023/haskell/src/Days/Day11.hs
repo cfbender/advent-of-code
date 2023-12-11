a91475fcda0505c7644f9326466d689f90492ac1
Cody Bender <cody.bender@pdq.com>
2023-12-11 01:26:44 -0500

    chore(2023): add parser and setup for day 11 part a

--- 2023/haskell/src/Days/Day11.hs
+++ 2023/haskell/src/Days/Day11.hs
@@ -4,6 +4,12 @@ module Days.Day11 where
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text (Parser, sepBy, endOfLine)
 import Data.Void
+import Data.List
+import Util.Parsers (coordinateParser)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Vector (Vector)
+import qualified Data.Vector as Vector
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -11,18 +17,36 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser =
+  Vector.fromList
+    . map (Vector.fromList . map snd)
+    . transpose
+    . groupBy (\((xA, _), _) ((xB, _), _) -> xA == xB)
+    . Map.toList
+    <$> coordinateParser space 0
+  where
+    space '#' = Just Galaxy
+    space '.' = Just Empty
 
 ------------ TYPES ------------
-type Input = Void
+data Space = Empty | Galaxy deriving (Show, Eq)
 
-type OutputA = Void
+type Coordinate = (Int, Int)
+
+type Input = Vector (Vector Space)
+
+type OutputA = String
 
 type OutputB = Void
 
 ------------ PART A ------------
+-- TODO: splitAt each empty, insert new empty row/col, then calculate manhattan distance on new grid
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA i = show emptyCols
+  where
+    transposed = Vector.fromList . map Vector.fromList . transpose . map Vector.toList $ Vector.toList i
+    emptyRows = Vector.filter (all (== Empty) . snd) $ Vector.indexed i
+    emptyCols = Vector.filter (all (== Empty) . snd) $ Vector.indexed transposed
 
 ------------ PART B ------------
 partB :: Input -> OutputB
