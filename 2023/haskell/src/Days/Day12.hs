450dd0d860894c4a0fd25848829a777ccf8fa93d
Cody Bender <cody.bender@pdq.com>
2023-12-12 11:52:25 -0500

    chore(2023): day 12 hs

--- 2023/haskell/src/Days/Day12.hs
+++ 2023/haskell/src/Days/Day12.hs
@@ -1,14 +1,15 @@
 module Days.Day12 where
 
 {- ORMOLU_DISABLE -}
-import Data.List 
+import Control.Monad
+import Data.List
 import qualified Data.List  as List
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text (Parser, sepBy, endOfLine, char, many1, choice, space, decimal)
 import Data.Void
-import Data.Vector
+import Data.Vector (Vector)
 import qualified Data.Vector as V
-import Data.Ord 
+import Data.Ord
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -19,43 +20,67 @@ inputParser :: Parser Input
 inputParser = springLine `sepBy` endOfLine
   where
     springLine = do
-      springs <- V.fromList . List.map spring <$> many1 (choice [char '#', char '.', char '?'])
+      springs <- many1 (choice [char '#', char '.', char '?'])
       space
       counts <- decimal `sepBy` char ','
       return (springs, counts)
-    spring '#' = Damaged
-    spring '.' = Operational
-    spring '?' = Unknown
 
 ------------ TYPES ------------
-data Condition = Operational | Damaged | Unknown deriving (Show, Eq)
 
-type Input = [(Vector Condition, [Int])]
+type Input = [(String, [Int])]
 
-type OutputA = String
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = String
 
 ------------ PART A ------------
--- ideas:
--- guess and check, write function that checks end state
--- (ie that #.#.### satisfies 1,1,3), and then check all possibilities
---      so for ???.###
---        try: ....###
---             ..#.###
---             etc..
---             ###.###
---        somewhere in there would've been #.#.###
---        binary so it's just 2^3 things to check right?
---        full input looks like maybe ~15 max. that's 32k things to check woof
---        so maybe find an optimization on this to limit the search space
---        note: have to check full search space since enumerating combinations that work
---
---
--- something with a sliding window matching the group?
+-- springs is binary rep of filled springs list
+-- groups is input (eg. 1,1,3)
+check :: [Int] -> [Int] -> Bool
+check groups springs = groups == sGroups
+  where
+    sGroups =
+      map sum
+        . filter (/= [0])
+        $ groupBy (\a b -> a == 1 && b == 1) springs
+
+-- decimal to binary list of 1 or 0
+binary :: Int -> [Int]
+binary 0 = [0]
+binary n = go n
+  where
+    go 0 = []
+    go k = mod k 2 : go (div k 2)
+
+-- n is number of ?s
+-- f is number missing
+combos :: Int -> Int -> [[Int]]
+combos n f = filter ((== f) . sum) $ replicateM n [0, 1]
+
+replace :: String -> [Int] -> [Int]
+replace [] [] = []
+replace ('?' : r) (x : r') = x : replace r r'
+replace ('.' : r) g = 0 : replace r g
+replace ('#' : r) g = 1 : replace r g
+
+ways springs groups =
+  length
+    . filter (check groups)
+    . map (replace springs)
+    $ combos unknowns missing
+  where
+    unknowns = length $ filter (== '?') springs
+    present = length $ filter (== '#') springs
+    missing = sum groups - present
+
 partA :: Input -> OutputA
-partA = show
+partA = sum . map (uncurry ways)
 
 ------------ PART B ------------
+unfold :: (String, [Int]) -> (String, [Int])
+unfold (s, g) = (intercalate "?" $ replicate 5 s, concat $ replicate 5 g)
+
+-- this will take like 18 trillion years.
+-- need to figure out DP/memoization on this
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = show . map (uncurry ways . unfold)
