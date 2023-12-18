1b4842c3838ad069fceec3577961947721cab328
Cody Bender <cody.bender@pdq.com>
2023-12-17 23:04:58 -0500

    chore(2023): day 17 hs start

--- 2023/haskell/src/Days/Day17.hs
+++ 2023/haskell/src/Days/Day17.hs
@@ -2,8 +2,15 @@ module Days.Day17 where
 
 {- ORMOLU_DISABLE -}
 import qualified Program.RunDay as R (runDay, Day)
-import Data.Attoparsec.Text (Parser, sepBy, endOfLine)
+import Data.Attoparsec.Text (Parser, sepBy, endOfLine, many1, digit)
 import Data.Void
+import Util.Parsers (Coordinate, coordinateParser)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.Char (digitToInt)
+import Util.Util ( mapBoundingBox, traceShowIdWithContext)
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -11,18 +18,77 @@ runDay = R.runDay inputParser part1 part2
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = coordinateParser mapper 0
+  where
+    mapper c = Just (digitToInt c)
 
 ------------ TYPES ------------
-type Input = Void
+type Input = Map Coordinate Int
 
-type OutputA = Void
+type OutputA = String
 
 type OutputB = Void
 
+type Costs = Map Coordinate Int
+
+data Direction = North | West | South | East deriving (Enum, Show, Eq)
+
+-- consider all directions equal for sorting
+instance Ord Direction where
+  compare x y = EQ
+
+type Location = (Int, Coordinate, (Direction, Int))
+
+-- the queue is tuples of the point, the distance to that point,
+-- and how many times it's gone in a certain direction
+type DijkstrasQueue = (Set Location, Map Coordinate Int)
+
 ------------ PART A ------------
+rev North = South
+rev South = North
+rev West = East
+rev East = West
+
+nextPoints :: Location -> [(Coordinate, (Direction, Int))]
+nextPoints (_, (x, y), (dir, n)) =
+  -- limit forward to 3 times same direction
+  -- and remove the option to turn around
+  filter
+    (\(_, (d, n')) -> d /= rev dir && n' <= 3)
+    $ map
+      -- if going in same direction as input, increment by that amount
+      (\p@(c, (d', x)) -> if d' == dir then (c, (d', x + n)) else p)
+      [ ((x - 1, y), (West, 1)),
+        ((x + 1, y), (East, 1)),
+        ((x, y - 1), (North, 1)),
+        ((x, y + 1), (South, 1))
+      ]
+
+-- re-implementing here to keep the extra data of num of straight
+dijkstras :: Input -> Int
+dijkstras i = dijkstras' (Set.singleton (0, (0, 0), (South, 0))) Map.empty
+  where
+    (_, maxX, _, maxY) = mapBoundingBox i
+    dest = (maxX, maxY)
+    isCheaper m (cost, p, d) = not (Map.member (p, d) m) || (cost < m Map.! (p, d))
+    -- costs is map of ((x,y), (dir, count)) -> cost
+    dijkstras' :: Set Location -> Map (Coordinate, (Direction, Int)) Int -> Int
+    dijkstras' candidates costs =
+      let (c@(cost, curr, _), rest) = Set.deleteFindMin candidates
+          candidates' =
+            filter (isCheaper costs)
+              . map (\(loc, d) -> (cost + (i Map.! loc), loc, d))
+              . filter (flip Map.member i . fst)
+              $ nextPoints c
+          newCandidates = Set.union (Set.fromList candidates') rest
+          candidateCosts = map (\(cost, coord, d) -> ((coord, d), cost)) candidates'
+          newCosts = foldr (uncurry Map.insert) costs candidateCosts
+       in if curr == dest
+            then cost
+            else dijkstras' newCandidates newCosts
+
 part1 :: Input -> OutputA
-part1 = error "Not implemented yet!"
+part1 = show . dijkstras
 
 ------------ PART B ------------
 part2 :: Input -> OutputB
