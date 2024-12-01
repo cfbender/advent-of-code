5062ae2014510a8df5c31c5282c70b17b0c85919
Cody Bender <cody.bender@pdq.com>
2024-12-01 12:02:39 -0500

    chore: init 2024 hs

---
+++ 2024/haskell/src/Days/Day03.hs
@@ -0,0 +1,39 @@
+module Days.Day03 (runDay) where
+
+{- ORMOLU_DISABLE -}
+import Data.List
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import Data.Maybe
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.Vector (Vector)
+import qualified Data.Vector as Vec
+import qualified Util.Util as U
+
+import qualified Program.RunDay as R (runDay, Day)
+import Data.Attoparsec.Text
+import Data.Void
+{- ORMOLU_ENABLE -}
+
+runDay :: R.Day
+runDay = R.runDay inputParser partA partB
+
+------------ PARSER ------------
+inputParser :: Parser Input
+inputParser = error "Not implemented yet!"
+
+------------ TYPES ------------
+type Input = Void
+
+type OutputA = Void
+
+type OutputB = Void
+
+------------ PART A ------------
+partA :: Input -> OutputA
+partA = error "Not implemented yet!"
+
+------------ PART B ------------
+partB :: Input -> OutputB
+partB = error "Not implemented yet!"
