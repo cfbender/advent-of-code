3d060cde4a7a17f9474c143e0781f7f854814221
Cody Bender <cody.bender@pdq.com>
2024-12-02 12:09:54 -0500

    feat: day 2 2024 hs

--- 2024/haskell/src/Days/Day02.hs
+++ 2024/haskell/src/Days/Day02.hs
@@ -1,19 +1,17 @@
 module Days.Day02 (runDay) where
 
+-- Not a bad day 2, pretty fun one! Still just a lot of time fighting the compiler
+-- as I ride the bumpy start of learning Haskell again. Loving it tho, it writes such pretty code
+-- even when my example here is pretty ugly.
+-- Onward!
+
 {- ORMOLU_DISABLE -}
 import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
 import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
 
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
-import Data.Void
+import Data.Ix
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -21,19 +19,48 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = filter ((> 1) . length) <$> report `sepBy` endOfLine
+ where
+  report = decimal `sepBy` char ' '
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [[Int]]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
+
+data Finding = Safe | Unsafe
+  deriving (Show, Eq)
 
 ------------ PART A ------------
+valid :: Int -> Int -> Bool
+valid x y = inRange (1, 3) (abs (x - y))
+
+check :: Finding -> [Int] -> Finding
+check Unsafe _ = Unsafe
+check Safe (x : y : z : xs) = check safety (y : z : xs)
+ where
+  safe = valid x y && valid y z
+  safety = if safe && ((z > y && y > x) || (z < y && y < x)) then Safe else Unsafe
+-- getting to a 2 length list means we've compared all the windows
+check Safe [_, _] = Safe
+
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA = length . filter (== Safe) . map (check Safe)
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB input = length $ mapMaybe (find (== Safe)) results
+ where
+  results :: [[Finding]]
+  results = map (map (check Safe)) tests
+  tests :: [[[Int]]]
+  -- get all the permutations of removing an element
+  tests = map permute input
+  permute :: [Int] -> [[Int]]
+  -- generate combinations including the original list to check first
+  permute xs = xs : [deleteAt n xs | n <- [0 .. length xs - 1]]
+  deleteAt n xs =
+    let (ys, zs) = splitAt n xs
+     in ys ++ tail zs
