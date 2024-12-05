29910444f854a2fc3172b9982ea00434792833c5
Cody Bender <cody.bender@pdq.com>
2024-12-05 12:59:22 -0500

    feat: day 5 hs 2024

--- 2024/haskell/test/Days/Day05Spec.hs
+++ 2024/haskell/test/Days/Day05Spec.hs
@@ -9,11 +9,11 @@ spec :: Spec
 spec = do
   describe "Day 05" $ do
     it "returns the correct answer for the test input for part 1" $ do
-      let testText = ""
+      let testText = "47|53\n97|13\n97|61\n97|47\n75|29\n61|13\n75|53\n29|13\n97|29\n53|29\n61|53\n97|53\n61|29\n47|13\n75|47\n97|75\n47|61\n75|61\n47|29\n75|13\n53|13\n\n75,47,61,53,29\n97,61,53,29,13\n75,29,13\n75,97,47,61,53\n61,13,29\n97,13,75,29,47"
       let Just testInput = parseTest inputParser testText
-      part1 testInput `shouldBe` undefined
+      part1 testInput `shouldBe` (143 :: Int)
 
     it "returns the correct answer for the test input for part 2" $ do
-      let testText = ""
+      let testText = "47|53\n97|13\n97|61\n97|47\n75|29\n61|13\n75|53\n29|13\n97|29\n53|29\n61|53\n97|53\n61|29\n47|13\n75|47\n97|75\n47|61\n75|61\n47|29\n75|13\n53|13\n\n75,47,61,53,29\n97,61,53,29,13\n75,29,13\n75,97,47,61,53\n61,13,29\n97,13,75,29,47"
       let Just testInput = parseTest inputParser testText
-      part2 testInput `shouldBe` undefined
+      part2 testInput `shouldBe` (123 :: Int)
