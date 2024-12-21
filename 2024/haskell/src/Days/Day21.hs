43813f151091b63e61298e299b3fad9c0ecd984e
Cody Bender <cody.bender@pdq.com>
2024-12-21 14:27:48 -0500

    chore: 2024 day 21 hs parse

--- 2024/haskell/src/Days/Day21.hs
+++ 2024/haskell/src/Days/Day21.hs
@@ -1,6 +1,6 @@
 module Days.Day21 where
 
-import Data.Attoparsec.Text (Parser, endOfLine, sepBy)
+import Data.Attoparsec.Text (Parser, anyChar, char, choice, digit, endOfLine, letter, many1, sepBy)
 import Data.Void
 import Program.RunDay qualified as R (Day, runDay)
 
@@ -9,18 +9,47 @@ runDay = R.runDay inputParser part1 part2
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = instructions `sepBy` endOfLine
+ where
+  instructions = do
+    instructions <- many1 (choice [digit, char 'A'])
+    return (map instruction instructions)
+  instruction '0' = Zero
+  instruction '1' = One
+  instruction '2' = Two
+  instruction '3' = Three
+  instruction '4' = Four
+  instruction '5' = Five
+  instruction '6' = Six
+  instruction '7' = Seven
+  instruction '8' = Eight
+  instruction '9' = Nine
+  instruction 'A' = Push
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [[NumInstruction]]
 
-type OutputA = Void
+type OutputA = String
 
 type OutputB = Void
 
+data NumInstruction
+  = Zero
+  | One
+  | Two
+  | Three
+  | Four
+  | Five
+  | Six
+  | Seven
+  | Eight
+  | Nine
+  | Push
+  deriving (Show, Eq)
+
 ------------ PART 1 ------------
 part1 :: Input -> OutputA
-part1 = error "Not implemented yet!"
+part1 = show
 
 ------------ PART 2 ------------
 part2 :: Input -> OutputB
--- 2024/haskell/test/Days/Day21Spec.hs
+++ 2024/haskell/test/Days/Day21Spec.hs
@@ -9,9 +9,9 @@ spec :: Spec
 spec = do
   describe "Day 21" $ do
     it "returns the correct answer for the test input for part 1" $ do
-      let testText = ""
+      let testText = "029A\n980A\n179A\n456A\n379A"
       let Just testInput = parseTest inputParser testText
-      part1 testInput `shouldBe` undefined
+      part1 testInput `shouldBe` 126384
 
     it "returns the correct answer for the test input for part 2" $ do
       let testText = ""
