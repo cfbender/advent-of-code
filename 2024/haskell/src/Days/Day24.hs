c0a00b235fc7ebdd73be4df0fab69849b848449c
Cody Bender <cody.bender@pdq.com>
2025-01-05 20:44:29 -0500

    feat: 2024 day 24 hs part 1

--- 2024/haskell/src/Days/Day24.hs
+++ 2024/haskell/src/Days/Day24.hs
@@ -1,6 +1,15 @@
 module Days.Day24 where
 
-import Data.Attoparsec.Text (Parser, endOfLine, sepBy)
+-- crazy part 2, don't feel like dealing with having to check 2^46 numbers.
+-- obviously some trick, don't wanna figure it out
+-- pretty happy with part 1 tho! even though it's a little late. fun plane activity
+
+import Data.Attoparsec.Text (Parser, anyChar, char, choice, count, decimal, endOfLine, many1, sepBy, string)
+import Data.Bits (xor, (.&.), (.|.))
+import Data.List (isPrefixOf, sort, sortBy)
+import Data.Map (Map)
+import Data.Map qualified as Map
+import Data.Ord (Down (..), comparing)
 import Data.Void
 import Program.RunDay qualified as R (Day, runDay)
 
@@ -9,19 +18,67 @@ runDay = R.runDay inputParser part1 part2
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = do
+  states <- state `sepBy` endOfLine
+  count 2 endOfLine
+  gates <- gate `sepBy` endOfLine
+  return (Map.fromList states, gates)
+  where
+    state = do
+      name <- count 3 anyChar
+      string ": "
+      value <- decimal
+      return (name, value)
+    gate = do
+      first <- count 3 anyChar
+      char ' '
+      gateType <- gateType' <$> choice [string "AND", string "OR", string "XOR"]
+      char ' '
+      second <- count 3 anyChar
+      string " -> "
+      output <- count 3 anyChar
+      return (Gate (first, second) output gateType)
+    gateType' "AND" = AND
+    gateType' "OR" = OR
+    gateType' "XOR" = XOR
 
 ------------ TYPES ------------
-type Input = Void
+type Input = (Map String Int, [Gate])
 
-type OutputA = Void
+type OutputA = Int
 
 type OutputB = Void
 
+data Operation = AND | OR | XOR deriving (Show, Eq)
+
+data Gate = Gate
+  { inputs :: (String, String),
+    output :: String,
+    operation :: Operation
+  }
+  deriving (Show, Eq)
+
 ------------ PART 1 ------------
+getOutput :: Map String Int -> Map String Gate -> String -> Int
+getOutput state gates wire = case Map.lookup wire state of
+  Just value -> value
+  Nothing -> case Map.lookup wire gates of
+    Just gate -> case gate of
+      Gate (a, b) _ AND -> getOutput state gates a .&. getOutput state gates b
+      Gate (a, b) _ OR -> getOutput state gates a .|. getOutput state gates b
+      Gate (a, b) _ XOR -> getOutput state gates a `xor` getOutput state gates b
+    Nothing -> error $ "Could not find wire " ++ wire
+
+convert :: [Int] -> Int
+convert [] = 0
+convert (x : xs) = x + 2 * convert xs
+
 part1 :: Input -> OutputA
-part1 = error "Not implemented yet!"
+part1 (state, gates) = convert $ map (getOutput state byOutput) digitWires
+  where
+    byOutput = Map.fromList $ map (\g -> (output g, g)) gates
+    digitWires = sort . filter ((== 'z') . head) $ map output gates
 
 ------------ PART 2 ------------
 part2 :: Input -> OutputB
-part2 = error "Not implemented yet!"
+part2 (state, gates) = error "Not implemented yet!"
--- 2024/haskell/test/Days/Day24Spec.hs
+++ 2024/haskell/test/Days/Day24Spec.hs
@@ -9,11 +9,12 @@ spec :: Spec
 spec = do
   describe "Day 24" $ do
     it "returns the correct answer for the test input for part 1" $ do
-      let testText = ""
+      let testText = "x00: 1\nx01: 0\nx02: 1\nx03: 1\nx04: 0\ny00: 1\ny01: 1\ny02: 1\ny03: 1\ny04: 1\n\nntg XOR fgs -> mjb\ny02 OR x01 -> tnw\nkwq OR kpj -> z05\nx00 OR x03 -> fst\ntgd XOR rvg -> z01\nvdt OR tnw -> bfw\nbfw AND frj -> z10\nffh OR nrd -> bqk\ny00 AND y03 -> djm\ny03 OR y00 -> psh\nbqk OR frj -> z08\ntnw OR fst -> frj\ngnj AND tgd -> z11\nbfw XOR mjb -> z00\nx03 OR x00 -> vdt\ngnj AND wpb -> z02\nx04 AND y00 -> kjc\ndjm OR pbm -> qhw\nnrd AND vdt -> hwm\nkjc AND fst -> rvg\ny04 OR y02 -> fgs\ny01 AND x02 -> pbm\nntg OR kjc -> kwq\npsh XOR fgs -> tgd\nqhw XOR tgd -> z09\npbm OR djm -> kpj\nx03 XOR y03 -> ffh\nx00 XOR y04 -> ntg\nbfw OR bqk -> z06\nnrd XOR fgs -> wpb\nfrj XOR qhw -> z04\nbqk OR frj -> z07\ny03 OR x01 -> nrd\nhwm AND bqk -> z03\ntgd XOR rvg -> z12\ntnw OR pbm -> gnj"
       let Just testInput = parseTest inputParser testText
-      part1 testInput `shouldBe` undefined
+      part1 testInput `shouldBe` 2024
 
     it "returns the correct answer for the test input for part 2" $ do
-      let testText = ""
+      let testText = "x00: 0\nx01: 1\nx02: 0\nx03: 1\nx04: 0\nx05: 1\ny00: 0\ny01: 0\ny02: 1\ny03: 1\ny04: 0\ny05: 1\n\nx00 AND y00 -> z05\nx01 AND y01 -> z02\nx02 AND y02 -> z01\nx03 AND y03 -> z03\nx04 AND y04 -> z04\nx05 AND y05 -> z00"
       let Just testInput = parseTest inputParser testText
+      -- part2 testInput `shouldBe` "z00,z01,z02,z05"
       part2 testInput `shouldBe` undefined
