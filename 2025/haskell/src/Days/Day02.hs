71e81ec775aba0a310cf632d2ca60fe23014f380
Cody Bender <cody.bender@pdq.com>
2025-12-02 12:37:16 -0500

    feat: day 2 2025 hs

--- 2025/haskell/src/Days/Day02.hs
+++ 2025/haskell/src/Days/Day02.hs
@@ -1,7 +1,9 @@
 module Days.Day02 where
 
-import Data.Attoparsec.Text (Parser, endOfLine, sepBy)
-import Data.Void
+-- day 2! Got tripped up on part 2, and ended up rewriting it so many times until this much simpler solution.
+-- classic case of overcomplicating it really.
+
+import Data.Attoparsec.Text (Parser, char, decimal, sepBy)
 import Program.RunDay qualified as R (Day, runDay)
 
 runDay :: R.Day
@@ -9,19 +11,49 @@ runDay = R.runDay inputParser part1 part2
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = range `sepBy` char ','
+ where
+  range = do
+    start <- decimal
+    _ <- char '-'
+    end <- decimal
+    return (start, end)
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [(Int, Int)]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART 1 ------------
+-- Check if a number is made of two identical halves
+invalid :: Int -> Bool
+invalid num =
+  let s = show num
+      n = length s
+   in even n && take (n `div` 2) s == drop (n `div` 2) s
+
 part1 :: Input -> OutputA
-part1 = error "Not implemented yet!"
+part1 = sum . concatMap filterInvalid
+ where
+  filterInvalid :: (Int, Int) -> [Int]
+  filterInvalid (start, end) = [n | n <- [start .. end], invalid n]
 
 ------------ PART 2 ------------
+-- Check if a number is made of a pattern repeated at least twice
+invalid2 :: Int -> Bool
+invalid2 num = any isRepeatingPattern [1 .. length s `div` 2]
+ where
+  s = show num
+  isRepeatingPattern patternLen =
+    length s `mod` patternLen == 0
+      && let pattern = take patternLen s
+             numRepeats = length s `div` patternLen
+          in numRepeats >= 2 && concat (replicate numRepeats pattern) == s
+
 part2 :: Input -> OutputB
-part2 = error "Not implemented yet!"
+part2 = sum . concatMap filterInvalid2
+ where
+  filterInvalid2 :: (Int, Int) -> [Int]
+  filterInvalid2 (start, end) = [n | n <- [start .. end], invalid2 n]
--- 2025/haskell/test/Days/Day02Spec.hs
+++ 2025/haskell/test/Days/Day02Spec.hs
@@ -9,11 +9,11 @@ spec :: Spec
 spec = do
   describe "Day 02" $ do
     it "returns the correct answer for the test input for part 1" $ do
-      let testText = ""
+      let testText = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
       let Just testInput = parseTest inputParser testText
-      part1 testInput `shouldBe` undefined
+      part1 testInput `shouldBe` 1227775554
 
     it "returns the correct answer for the test input for part 2" $ do
-      let testText = ""
+      let testText = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
       let Just testInput = parseTest inputParser testText
-      part2 testInput `shouldBe` undefined
+      part2 testInput `shouldBe` 4174379265
