5cea232838b915725dc98bb70c8e503547dee691
Cody Bender <cody.bender@pdq.com>
2025-12-05 13:55:54 -0500

    chore: 2025 day 5 hs

--- 2025/haskell/src/Days/Day05.hs
+++ 2025/haskell/src/Days/Day05.hs
@@ -1,7 +1,12 @@
 module Days.Day05 where
 
-import Data.Attoparsec.Text (Parser, endOfLine, sepBy)
-import Data.Void
+-- Data.Range coming in huge here, took me a while to get the bounds before I figured out
+-- I could just pattern match it out like I did here.
+-- Made it super easy, though I think the joinRanges wouldn't have been that hard to write otherwise
+
+import Data.Attoparsec.Text (Parser, char, count, decimal, endOfLine, sepBy)
+import Data.Range (Bound (..), BoundType (..), Range (SpanRange), (+=+))
+import Data.Range qualified as Range
 import Program.RunDay qualified as R (Day, runDay)
 
 runDay :: R.Day
@@ -9,19 +14,37 @@ runDay = R.runDay inputParser part1 part2
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = do
+    ranges <- range `sepBy` endOfLine
+    count 2 endOfLine
+    ingredients <- decimal `sepBy` endOfLine
+    return (ranges, ingredients)
+  where
+    range = do
+        start <- decimal
+        char '-'
+        end <- decimal
+        return (start +=+ end)
 
 ------------ TYPES ------------
-type Input = Void
+type Input = ([Range Int], [Int])
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART 1 ------------
+-- 389 too low
 part1 :: Input -> OutputA
-part1 = error "Not implemented yet!"
+part1 (ranges, ingredients) = length [i | i <- ingredients, Range.inRanges allRanges i]
+  where
+    allRanges = Range.mergeRanges ranges
 
 ------------ PART 2 ------------
 part2 :: Input -> OutputB
-part2 = error "Not implemented yet!"
+part2 (ranges, _) = sum $ map (\(x, y) -> y - x + 1) allRanges
+  where
+    -- merge all ranges to knock out overlaps
+    allRanges = map toBounds $ Range.joinRanges ranges
+    toBounds :: Range Int -> (Int, Int)
+    toBounds (SpanRange (Bound x Inclusive) (Bound y Inclusive)) = (x, y)
--- 2025/haskell/test/Days/Day05Spec.hs
+++ 2025/haskell/test/Days/Day05Spec.hs
@@ -9,11 +9,11 @@ spec :: Spec
 spec = do
     describe "Day 05" $ do
         it "returns the correct answer for the test input for part 1" $ do
-            let testText = ""
+            let testText = "3-5\n10-14\n16-20\n12-18\n\n1\n5\n8\n11\n17\n32"
             let Just testInput = parseTest inputParser testText
-            part1 testInput `shouldBe` undefined
+            part1 testInput `shouldBe` 3
 
         it "returns the correct answer for the test input for part 2" $ do
-            let testText = ""
+            let testText = "3-5\n10-14\n16-20\n12-18\n\n1\n5\n8\n11\n17\n32"
             let Just testInput = parseTest inputParser testText
-            part2 testInput `shouldBe` undefined
+            part2 testInput `shouldBe` 14
